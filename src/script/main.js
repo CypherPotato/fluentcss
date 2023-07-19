b64c7375a616634d33e390bce5a9420f16188b06
CypherPotato <test@gmail.com>
2023-07-19 15:51:54 -0300

    v.0.3

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,9 @@
+const __winroot = document.querySelector(":root");
 document.addEventListener('mousemove', (e) => {
     window.mouseX = e.clientX;
     window.mouseY = e.clientY;
+
+    __winroot.style.setProperty('--window-x', e.clientX + "px");
+    __winroot.style.setProperty('--window-y', e.clientY + "px");
 });
 window.win = {};
